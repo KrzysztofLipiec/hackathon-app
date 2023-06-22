f66579eb8adcc6fea354299378155008b7c9e177
Krzysztof <krzysztof.lipiec@appfire.com>
2023-06-22 12:59:50 +0100

    rebuild prompt

--- src/gpt/prompts/assessment.prompt.ts
+++ src/gpt/prompts/assessment.prompt.ts
@@ -20,7 +20,7 @@ export class AssessmentPrompt extends AbstractPrompt {
         });
         return `
         Write an assessment review for our employee 
-        ${user} based on the following data of issues solved by the employee. Here are the reviews from the clients:
+        ${user} based on the following data of issues solved by the employee. Here are the reviews from the clients and conversations:
         ${assessment.join('\n')}
         Please let me know how the employee is doing and if the employee is solving user's problems. Please give me a list of strengths and areas to improve and finish it off with a short summary.
         At the beginning please give me the information such as name of employee and today's date.
